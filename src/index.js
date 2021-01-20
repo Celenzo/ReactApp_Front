4055c11589bcb1aca14ea3abf464109503f6911b
celenzo <so6master@gmail.com>
2021-01-21 00:04:18 +0100

    feat(styling first version)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//Styling
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
+//Bootstrap elements
+import Spinner from 'react-bootstrap/Spinner';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Toast from 'react-bootstrap/Toast';
+ 
 class ConvList extends React.Component {
 
     constructor(props) {
@@ -87,7 +95,8 @@ class ConvList extends React.Component {
         fetch('http://0.0.0.0:8568/conversations', requestOptions)
             .then(response => response.json())
             .then(res => this.setState(prevState => ({
-                convListItems: [...prevState.convListItems, res]
+                convListItems: [...prevState.convListItems, res],
+                newConvInputValue: ''
             })), err => {
                 this.error = err;
             });
@@ -109,7 +118,8 @@ class ConvList extends React.Component {
         fetch(`http://0.0.0.0:8568/conversations/${this.state.selectedConvId}`, requestOptions)
             .then(res => res.json())
             .then(res => this.setState(prevState => ({
-                messageList: [...prevState.messageList, res]
+                messageList: [...prevState.messageList, res],
+                msgInputValue: ''
             })));
     }
 
@@ -127,25 +137,47 @@ class ConvList extends React.Component {
 
     render() {
         const { error, loading, convListItems, messageList } = this.state;
-        if (error) return <div>Error: { error.message } </div>
-        if (loading) return <div>Chargement...</div>
+        
+        if (error) return (
+            <Toast>
+                <Toast.Header><strong className="mr-auto">Error</strong></Toast.Header>
+                <Toast.Body>An error has occured: {this.state.error}</Toast.Body>
+            </Toast>
+        )
+
+        if (loading) return (
+            <Spinner animation="border" role="status" className='loadingSpinner'>
+                <span className="sr-only">Loading...</span>
+            </Spinner>
+        )
+        
         return (
-            <div>
-                <div>
-                    <input type='text' id='convNameInput' value={this.newConvInputValue} onChange={this.handleConvNameInputChange}></input>
-                    <button key='newConvBtn' id='newConvBtn' onClick={this.newConv}>NEW</button>
+            <div className='appWrapper'>
+
+                <div className='newConvWrapper'>
+                    <Form.Control type='text' id='convNameInput' className='newConvInput' value={this.newConvInputValue} onChange={this.handleConvNameInputChange}></Form.Control>
+                    <Button variant='outline-primary' key='newConvBtn' id='newConvBtn' className='newConvBtn' onClick={this.newConv}>NEW</Button>
                 </div>
-                <button key='closeConvBtn' id='closeConvBtn' onClick={this.closeConv}>CLOSE</button>
-                <ul>
-                    { convListItems.map(conv => <li key={conv.id} id={conv.id} onClick={this.loadMessages}>{conv.name}</li>) }
-                </ul>
-                <ul>
-                    { messageList.map(msgElem => <li key={msgElem.id} id={msgElem.id}>{msgElem.msg}</li>) }
-                </ul>
-                <div>
-                    <input type='text' id='messageInput' value={this.msgInputValue} onChange={this.handleMessageInputChange}></input>
-                    <button key='newMsgBtn' id='newMsgBtn' onClick={this.sendMsg}>SEND</button>
+
+                <div className='sideBarWrapper'>
+                    <ListGroup> { convListItems.map(conv =>
+                        <ListGroup.Item key={conv.id} id={conv.id} onClick={this.loadMessages} active={this.state.selectedConvId === conv.id ? true : false} >
+                            {conv.name}
+                        </ListGroup.Item>
+                    )} </ListGroup>
                 </div>
+
+                <div className='messageListWrapper'>
+                    <Button variant='danger' key='closeConvBtn' id='closeConvBtn' className="closeBtn" onClick={this.closeConv}>CLOSE</Button>
+                    <ListGroup  className='msgList' variant='flush'>
+                        { messageList.map(msgElem => <ListGroup.Item key={msgElem.id} id={msgElem.id}>{msgElem.msg}</ListGroup.Item>) }
+                    </ListGroup>
+                    <div className='newMsgFormWrapper'>
+                        <Form.Control as='textarea' rows={4} type='text' id='messageInput' className='msgInput' value={this.msgInputValue} onChange={this.handleMessageInputChange}></Form.Control>
+                        <Button key='newMsgBtn' id='newMsgBtn' className='sendBtn' onClick={this.sendMsg}>SEND</Button>
+                    </div>
+                </div>
+
             </div>
         )
     }
